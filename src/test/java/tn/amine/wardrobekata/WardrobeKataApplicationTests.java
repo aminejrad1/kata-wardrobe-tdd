5ec6552f18331fdc7cbb40b0f3bb64357f4b8f23
aminejrad1 <mohamedamine.jrad@esprit.tn>
2022-07-22 16:45:46 +0100

    added gitignore

---
+++ src/test/java/tn/amine/wardrobekata/WardrobeKataApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.amine.wardrobekata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardrobeKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
